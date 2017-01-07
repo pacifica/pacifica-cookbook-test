558aae43a82bc82358a1cddfc385fc4251cef919
David Brown <dmlb2000@gmail.com>
2017-01-07 15:46:07 -0800

    try getting some elastic load balancing with varnish

---
+++ recipes/elasticlb.rb
@@ -0,0 +1,12 @@
+es_endpoints = []
+search(
+  :node,
+  node['pacifica-integration-test']['elastic_search'],
+  filter_result: { ipaddress: ['ipaddress'] }
+).each do |esnode|
+  es_endpoints.push("#{esnode['ipaddress']}:9200")
+end
+pacifica_varnish 'default' do
+  backend_hosts: es_endpoints
+  listen_port: 9200
+end
