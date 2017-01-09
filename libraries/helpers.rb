202a1ebc800926a1403cdb054a2cef7c60d6c75e
David Brown <dmlb2000@gmail.com>
2017-01-09 13:00:42 -0800

    try better ruby helpers bits

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -1,31 +1,26 @@
-module PacificaIntegrationCookbook
-  def _factory_ipaddress(search)
-    def wrapped
+def module_name
+  self.class.to_s.split("::").first
+end
+%w(
+  core
+  elasticsearch_lb
+  elasticsearch_master
+  elasticsearch_slave
+  ingest
+  pgsql
+).each do |role_suffix|
+  str_ruby_method = %Q{
+    def #{role_suffix}_ipaddress
       ret = '127.0.0.1'
       search(
         :node,
-        "chef_environment:#{node.chef_environment} AND roles:pacifica_#{search}",
+        "chef_environment:#{node.chef_environment} AND roles:pacifica_#{role_suffix}",
         filter_result: { ipaddress: ['ipaddress'] }
       ).each do |result|
         ret = result['ipaddress']
       end
       ret
     end
-    wrapped
-  end
-  module PacificaIntegrationHelpers
-  end
-  %w(
-    core
-    elasticsearch_lb
-    elasticsearch_master
-    elasticsearch_slave
-    ingest
-    pgsql
-  ).each do |role_suffix|
-    ruby_method = %q{
-      #{role_suffix}_ipaddress = _factory_ipaddress(#{role_suffix})
-    }
-    PacificaIntegrationHelpers.module_eval(ruby_method)
-  end
+  }
+  module_name.method_eval(str_ruby_method)
 end
