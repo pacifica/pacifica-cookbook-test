0630acd31378bd69d70eceb7084749e372b4aa61
David Brown <dmlb2000@gmail.com>
2017-01-12 00:23:37 -0800

    reporting should be one more port

--- recipes/reporting.rb
+++ recipes/reporting.rb
@@ -1,5 +1,5 @@
 reporting_env = {
-  listen: "#{node['ipaddress']}:9000",
+  listen: "#{node['ipaddress']}:9001",
   additional_config: {
     "env[METADATA_PORT]" => "tcp://#{core_ipaddress}:8121",
     "env[POLICY_PORT]" => "tcp://#{core_ipaddress}:8181",
