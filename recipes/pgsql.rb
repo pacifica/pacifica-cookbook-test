ed0624fe429ffc9bcfec342ada9bdc1858b1d2bf
dmlb2000 <dmlb2000@gmail.com>
2017-02-06 13:25:45 -0800

    try to get more of the standard access methods in pg_hba

--- recipes/pgsql.rb
+++ recipes/pgsql.rb
@@ -7,8 +7,28 @@ node.default['postgresql']['pg_hba'] = [
     type: 'local',
     db: 'all',
     user: 'postgres',
-    method: 'trust',
-  }
+    method: 'ident',
+  },
+  {
+    type: 'local',
+    db: 'all',
+    user: 'all',
+    method: 'ident',
+  },
+  {
+    type: 'host',
+    db: 'all',
+    user: 'all',
+    addr: '127.0.0.1/32',
+    method: 'md5',
+  },
+  {
+    type: 'host',
+    db: 'all',
+    user: 'all',
+    addr: '::1/128',
+    method: 'md5',
+  },
 ]
 
 postgresql_connection_info = {
