858cd2a4e4652baf4a5493c1feb24eec1df3351f
David Brown <dmlb2000@gmail.com>
2017-02-23 20:03:54 -0800

    add proxy service to ingest box

---
+++ recipes/proxy.rb
@@ -0,0 +1,9 @@
+proxy_env = {
+  environment: {
+    METADATA_PORT: "tcp://#{core_ipaddress}:8121",
+    ARCHIVEI_PORT: "tcp://#{core_ipaddress}:8080",
+  },
+}
+pacifica_proxy 'proxy' do
+  service_opts proxy_env
+end
